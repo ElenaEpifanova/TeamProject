0cdff24032f78879734ce62f5fec664a14ee04e5
ice_kus <m.laym@bk.ru>
2020-11-24 19:39:12 +0400

    3

---
+++ TeamProject/Data/mocks/MockRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamProject.Data.interfaces;
+using TeamProject.Data.Models;
+
+namespace TeamProject.Data.mocks
+{
+    public class MockRequest : IRequest
+    {
+        private readonly ITechnic _technic = new MockTechnic();
+        public IEnumerable<Request> Requests
+        {
+            get
+            {
+                return new List<Request>
+                {
+                    new Request{id_Shop=1, id_Responsible=1, date_begin=0, date_end=1, description="", comment="", technic = new List<Technic> {_technic.Technics.First()} },
+                    new Request{id_Shop=2, id_Responsible=1, date_begin=0, date_end=1, description="", comment="", technic = new List<Technic> {_technic.Technics.First(), _technic.Technics.Last(), } }
+                };
+            }
+        }
+
+        public Request getObjectRequest(int requestId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TeamProject/Data/mocks/MockShop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamProject.Data.interfaces;
+using TeamProject.Data.Models;
+
+namespace TeamProject.Data.mocks
+{
+    public class MockShop : IShop
+    {
+        public IEnumerable<Shop> Shops
+        {
+            get
+            {
+                return new List<Shop>
+                {
+                    new Shop{name="КРС (Капитальный ремонт скважин)"},
+                    new Shop{name="ПРС (Подземный ремонт скважин)"}
+                };
+            }
+        }
+
+
+        public Shop getObjectTechnic(int shopId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
