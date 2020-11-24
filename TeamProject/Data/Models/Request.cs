e552e8d68b17e78827c58c14a056b4b20a120e87
ice_kus <m.laym@bk.ru>
2020-11-24 19:19:14 +0400

    2

---
+++ TeamProject/Data/Models/Request.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamProject.Data.Models
+{
+
+	public class Request
+	{
+		
+
+		public int id { set; get; }
+
+		public int id_Shop { set; get; }
+		public virtual Shop shop { set; get; }
+
+		
+		public int id_Responsible{ set; get; }
+		//public virtual Responsible responsible { set; get; }
+
+
+		public int date_begin { set; get; }  //type date
+		public int date_end { set; get; } //type date
+
+		public string description { set; get; }
+		public string comment { set; get; }
+
+		public List<Technic> technic { set; get; }
+
+	}
+
+}
---
+++ TeamProject/Data/interfaces/IRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamProject.Data.Models;
+
+namespace TeamProject.Data.interfaces
+{
+    interface IRequest
+    {
+        IEnumerable<Request> Requests { get; }
+        Request getObjectRequest(int requestId);
+    }
+}
