b0df416d348207d641767cea256e7222360e5b13
ElenaEpifanova <70274342+ElenaEpifanova@users.noreply.github.com>
2020-12-18 13:37:41 +0400

    diagram

--- TeamProject/wwwroot/js/graphic.js
+++ TeamProject/wwwroot/js/graphic.js
@@ -8,24 +8,15 @@ subgroupVisibility: { sg_1: true}}
 items.add(
 [{id: 1,
 content: 'Автогрейдер',
-start: '2020-12-17T08:00:00' ,
-end: '2020-12-21T11:00:00',
-group: 'bar',
-subgroup: 'sg_1',
-subgroupOrder: 0 },
-]);
-items.add(
-[{id: 2,
-content: 'Автокран',
-start: '2020-12-17T16:00:00' ,
-end: '2020-12-29T18:30:00',
+start: '2020-12-16T08:00:00' ,
+end: '2020-12-16T08:30:00',
 group: 'bar',
 subgroup: 'sg_1',
 subgroupOrder: 0 },
 ]);
 var container = document.getElementById('visualization'); 
 var options = { 
-start:'2020-12-16',
-end:'2020-12-31' ,
+start:'2020-12-15',
+end:'2020-12-19' ,
 };
 var timeline = new vis.Timeline(container, items, groups, options); 
